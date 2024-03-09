0d925b998f45b3160e8426c71eb441810cdd9b37
jasha.chec <jasha@chec.de>
2024-03-09 09:59:13 +0100

    feat(next.config): add configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    additionalData:
+      '@import "./styles/mixins.scss", "./styles/_variables.scss";',
+  },
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/PokeAPI/sprites/master/sprites/pokemon/**",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/PokeAPI/sprites/master/sprites/items/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
