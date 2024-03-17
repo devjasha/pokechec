28bd554f0b01ca56e35e74aa4b493c643f40d044
jasha.chec <jasha@chec.de>
2024-03-17 10:02:50 +0100

    feat(page): import pokemonDetails

--- app/[pokemon]/page.tsx
+++ app/[pokemon]/page.tsx
@@ -1,7 +1,15 @@
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import PokemonDetailStage from "@/components/components/modules/PokemonDetailStage";
+import { use } from "react";
+import { getPokemonDetails } from "@/components/utils/getPokemonDetails";
 
 const page = ({ params: pokemon }: Params) => {
-  return <h1>{pokemon.pokemon}</h1>;
+  let details = "";
+  if (pokemon.pokemon !== "favicon.ico") {
+    details = use(getPokemonDetails(pokemon.pokemon));
+  }
+
+  return <PokemonDetailStage pokemonDetails={details} />;
 };
 
 export default page;
